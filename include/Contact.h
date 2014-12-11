0489f9bdb6ab66187fdf1f72eec71f306d931056
Tizian Zeltner <tizian.zeltner@gmail.com>
2014-12-11 09:18:58 +0100

    + Contact struct
    Collision detection would ideally return a list of Contacts for easy collision response.
    (Maybe add flag to distinguish between contact points and THE contact points)

---
+++ include/Contact.h
@@ -0,0 +1,18 @@
+#pragma once
+
+// Baraff and Witkin approach
+class RigidBody;
+
+struct Contact {
+    RigidBody * a;  // for vertex-face case: RB with the vertex
+    RigidBody * b;  // for vertex-face case: RB with the face
+    
+    glm::vec3 p;    // point
+    
+    glm::vec3 n;    // normal pointing away from B. n = cross(ea, eb) for the edge-edge case
+    
+    glm::vec3 ea;   // edge of A for edge-edge case  (directions s.t. n is pointing away from B)
+    glm::vec3 eb;   // edge of B for edge-edge case
+    
+    bool vf;        // vertex-face case or not
+};
